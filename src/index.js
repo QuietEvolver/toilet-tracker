d97b90a728170b23f5bd1566343af6456d685752
Vera Weikel <vera.weikel@gmail.com>
2023-02-13 11:53:16 -0800

    Add ggl cluster markers.

--- src/index.js
+++ src/index.js
@@ -1,18 +1,47 @@
-
-// Initialize and add the map
+// import { MarkerClusterer } from "@googlemaps/markerclusterer";
+// ggl clustering: https://developers.google.com/maps/documentation/javascript/marker-clustering
+/* eslint-disable no-undef */
+/* eslint-enable no-undef */
 function initMap() {
-  // The location of Uluru
-  const uluru = { lat: -25.344, lng: 131.031 };
-  // The map, centered at Uluru
+  console.log(" I am in the initMap() function. ");
   const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 4,
-    center: uluru,
+    zoom: 3,
+    center: { lat: 45.520049472702055, lng: -122.67791084412895 },
   });
-  // The marker, positioned at Uluru
-  const marker = new google.maps.Marker({
-    position: uluru,
-    map: map,
+  const infoWindow = new google.maps.InfoWindow({
+    content: "",
+    disableAutoPan: true,
   });
+  // Create an array of alphabetical characters used to label the markers.
+  const labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  // Add some markers to the map.
+  const markers = locations.map((position, i) => {
+    const label = labels[i % labels.length];
+    const marker = new google.maps.Marker({
+      position,
+      label,
+    });
+
+    // markers can only be keyboard focusable when they have click listeners
+    // open info window when marker is clicked
+    marker.addListener("click", () => {
+      infoWindow.setContent(label);
+      infoWindow.open(map, marker);
+    });
+    return marker;
+  });
+
+  // Add a marker clusterer to manage the markers.
+  const markerClusterer = new markerClusterer.MarkerClusterer({ map, markers  });
 }
+
+const locations = [
+  { lat: 45.520049472702055, lng: -122.67791084412895 },
+  { lat: 45.53, lng: -122.67 },
+  { lat: 45.54, lng: -122.62 },
+  { lat: 45.55, lng: -122.64 },
+  { lat: 45.56, lng: -122.65 },
+
+];
 
 window.initMap = initMap;
